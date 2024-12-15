ecdd6bf3b8944f5fb521108e42091f47b61a7b59
Jainam Dedhia <128807846+JainamDedhia@users.noreply.github.com>
2024-12-15 15:43:17 +0530

    servicesel

--- gettoknowpage/android/app/src/main/kotlin/com/example/ewaste_management/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ewaste_management
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ servicessel/android/app/src/main/kotlin/com/example/servicessel/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.servicessel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
